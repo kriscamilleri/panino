a413c96d8b5a4e57cf092ed39e83a35252c7c9ca
kriscamilleri <kris.camilleri@gmail.com>
2025-01-26 16:25:19 +0100

    fixed tailwind classes not rendering on printout

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -337,7 +337,7 @@ async function handlePrint() {
         <html>
         <head>
             <title>${docStore.selectedFile.name}</title>
-            ${tailwindLink ? `<link rel="stylesheet" href="${tailwindLink.href}" />` : ''}
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" integrity="sha512-wnea99uKIC3TJF7v4eKk4Y+lMz2Mklv18+r4na2Gn1abDRPPOeef95xTzdwGD9e6zXJBteMIhZ1+68QC5byJZw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
             ${basePrintStyles}
         </head>
         <body>
