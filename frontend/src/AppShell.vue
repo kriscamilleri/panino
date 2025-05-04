264be9ed1754876d024983e95801840db9d5da7a
Kris Camilleri <kris.camilleri@gmail.com>
2025-05-04 18:10:37 +0200

    fixed scroll issue due to page rolading

--- frontend/src/AppShell.vue
+++ frontend/src/AppShell.vue
@@ -1,10 +1,11 @@
 <template>
-    <!-- Main outlet for page components --> <router-view v-slot="{ Component }">
-        <component :is="Component" :key="$route.fullPath" />
+    <!-- Main outlet for page components -->
+    <router-view v-slot="{ Component }">
+        <component :is="Component" />
     </router-view>
     <ToastContainer />
-
 </template>
+
 <script setup>
 import ToastContainer from '@/components/ToastContainer.vue'
 </script>
