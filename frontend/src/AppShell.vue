c285ae03858ef4d9a05d0cfa586498d3ae9e509d
Kris Camilleri <kris.camilleri@gmail.com>
2025-05-04 17:24:43 +0200

    debugging import error on build

--- frontend/src/AppShell.vue
+++ frontend/src/AppShell.vue
@@ -1,10 +1,10 @@
 <template>
     <!-- Main outlet for page components --> <router-view v-slot="{ Component }">
         <component :is="Component" :key="$route.fullPath" />
-    </router-view> <!-- Global toast container (bottom-right, auto-dismiss) -->
+    </router-view>
     <ToastContainer />
 
 </template>
 <script setup>
-import ToastContainer from './components/ToastContainer.vue'
+import ToastContainer from '@/components/ToastContainer.vue'
 </script>
